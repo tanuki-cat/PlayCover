096836199ee19fb0707feedd9f1319560ec17c31
Hao Guan <10684225+hguandl@users.noreply.github.com>
2025-04-11 18:07:50 +0800

    MaaTools

--- PlayCover/Model/AppSettings.swift
+++ PlayCover/Model/AppSettings.swift
@@ -42,6 +42,9 @@ struct AppSettingsData: Codable {
     var noKMOnInput = true
     var enableScrollWheel = true
 
+    var maaTools = false
+    var maaToolsPort = 1717
+
     init() {}
 
     // handle old 2.x settings where PlayChain did not exist yet
@@ -71,6 +74,9 @@ struct AppSettingsData: Codable {
         rootWorkDir = try container.decodeIfPresent(Bool.self, forKey: .rootWorkDir) ?? true
         noKMOnInput = try container.decodeIfPresent(Bool.self, forKey: .noKMOnInput) ?? true
         enableScrollWheel = try container.decodeIfPresent(Bool.self, forKey: .enableScrollWheel) ?? true
+
+        maaTools = try container.decodeIfPresent(Bool.self, forKey: .maaTools) ?? false
+        maaToolsPort = try container.decodeIfPresent(Int.self, forKey: .maaToolsPort) ?? 1717
     }
 }
 
--- PlayCover/Views/AppSettingsView.swift
+++ PlayCover/Views/AppSettingsView.swift
@@ -515,6 +515,21 @@ struct BypassesView: View {
                         .toggleStyle(.async($task, role: .iosFrameworks))
                     Spacer()
                 }
+                Spacer()
+                    .frame(height: 20)
+                HStack {
+                    Toggle("MaaTools", isOn: $settings.settings.maaTools)
+                    Spacer()
+                    Text("Port:")
+                    Stepper(value: $settings.settings.maaToolsPort, in: 1024 ... 65535) {
+                        TextField("MaaTools Port",
+                                  value: $settings.settings.maaToolsPort,
+                                  formatter: GraphicsView.number)
+                        .frame(width: 125)
+                    }
+                    .disabled(!settings.settings.maaTools)
+                    Spacer()
+                }
             }
             .padding()
         }
